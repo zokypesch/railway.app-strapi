034a5990ba0a4a069a6747e6776ba04af964b8fd
harry.sitohang <harry.sitohang@prakerja.go.id>
2024-11-12 16:12:44 +0700

    add collecltions

--- config/env/development/server.ts
+++ config/env/development/server.ts
@@ -1,3 +1,3 @@
-export default () => ({
-    url: 'http://127.0.0.1:1337'
+export default ({ env }) => ({
+    url: `http://127.0.0.1:${env('PORT')}`
 });
---
+++ src/api/course-tag/controllers/course-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * course-tag controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::course-tag.course-tag');
