9c97bad05dd9b06c55df744fc671898cff0e0f9e
zokypesch <maulanaoktofitriadi@gmail.com>
2024-11-13 19:58:44 +0700

    adding content security policy

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,18 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
