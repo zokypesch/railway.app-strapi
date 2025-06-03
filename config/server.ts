ceb6804ef747a11b77f6370397be5d7cced12bdc
zokypesch <maulanaoktofitriadi@gmail.com>
2025-06-03 13:00:01 +0700

    back deployment to old

--- config/server.ts
+++ config/server.ts
@@ -1,16 +1,33 @@
-const isBuild = process.argv.includes('build');
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('URL'),
+  proxy: true
+});
 
-const config = ({ env }) => {
-  const base = {
-    host: '0.0.0.0',
-    port: 1337,
-    app: {
-      keys: ['tj8ewf0h9w8zeadcbw726nl6k9xblr3v'],
-    },
-    proxy: true,
-  };
+// const isBuild = process.argv.includes('build');
 
-  return base;
-};
+// const config = ({ env }) => {
+//   const base = {
+//     host: '0.0.0.0',
+//     port: 1337,
+//     app: {
+//       keys: ['tj8ewf0h9w8zeadcbw726nl6k9xblr3v'],
+//     },
+//     proxy: true,
+//   };
 
-export default config;
+//   if (!isBuild) {
+//     return {
+//       ...base,
+//       url: 'https://cms.jaripmi.info',
+//     };
+//   }
+
+//   return base;
+// };
+
+// export default config;
