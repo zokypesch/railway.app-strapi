8b216a891840a17fa025a86521ad78bd61ebf0f3
zokypesch <maulanaoktofitriadi@gmail.com>
2025-06-03 12:49:08 +0700

    fixing docker

--- config/server.ts
+++ config/server.ts
@@ -5,17 +5,10 @@ const config = ({ env }) => {
     host: '0.0.0.0',
     port: 1337,
     app: {
-      keys: 'tj8ewf0h9w8zeadcbw726nl6k9xblr3v',
+      keys: ['tj8ewf0h9w8zeadcbw726nl6k9xblr3v'],
     },
     proxy: true,
   };
-
-  if (!isBuild) {
-    return {
-      ...base,
-      url: 'https://cms.jaripmi.info',
-    };
-  }
 
   return base;
 };
