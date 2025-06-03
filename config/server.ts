8337019bd324e3d5ed5d4fe9b4e7fbf447e7ffa6
zokypesch <maulanaoktofitriadi@gmail.com>
2025-06-03 12:32:21 +0700

    fixing docker

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,23 @@
 const isBuild = process.argv.includes('build');
 
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  url: isBuild ? undefined : env('URL', 'https://cms.jaripmi.info'),
-  proxy: true,
-});
+const config = ({ env }) => {
+  const base = {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    proxy: true,
+  };
+
+  if (!isBuild) {
+    return {
+      ...base,
+      url: env('URL', 'https://cms.jaripmi.info'),
+    };
+  }
+
+  return base;
+};
+
+export default config;
